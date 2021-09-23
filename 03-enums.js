25f679b6851dd43a6815f688d52242e72ec1e5f5
Calvin <zxw880507@gmail.com>
2021-09-23 16:36:25 -0600

    compiled js

---
+++ 03-enums.js
@@ -0,0 +1,30 @@
+var petOnSale = "chinchilla";
+var ordersArray = [
+    ["rat", 2],
+    ["chinchilla", 1],
+    ["hamster", 2],
+    ["chinchilla", 50],
+];
+// Write your code below:
+var Pet;
+(function (Pet) {
+    Pet[Pet["Hamster"] = 0] = "Hamster";
+    Pet[Pet["Rat"] = 1] = "Rat";
+    Pet[Pet["Chinchilla"] = 2] = "Chinchilla";
+    Pet[Pet["Tarantula"] = 3] = "Tarantula";
+})(Pet || (Pet = {}));
+var petOnSaleTS = Pet.Chinchilla;
+var ordersArrayTS = [
+    [Pet.Rat, 2],
+    [Pet.Chinchilla, 1],
+    [Pet.Hamster, 2],
+    [Pet.Chinchilla, 50],
+];
+// String Enums vs. Numeric Enums
+var PetString;
+(function (PetString) {
+    PetString["Hamster"] = "HAMSTER";
+    PetString["Rat"] = "RAT";
+    PetString["Chinchilla"] = "CHINCHILLA";
+    PetString["Tarantula"] = "TARANTULA";
+})(PetString || (PetString = {}));
