e767cd12ced7bab12e362463ca4250395c59213a
Calvin <zxw880507@gmail.com>
2021-09-27 17:28:38 -0600

    js complied

---
+++ dist/misson/11-easy-tuple-to-object.js
@@ -0,0 +1,10 @@
+// Give an array, transform into an object type and the key/value must in the given array.
+// For example
+const tuple = ["tesla", "model 3", "model X", "model Y"];
+const result = {
+    tesla: "tesla",
+    "model 3": "model 3",
+    "model X": "model X",
+    "model Y": "model Y",
+};
+// expected { tesla: 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y'}
