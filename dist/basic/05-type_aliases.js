8d5e2ec2f3094b3b27b79527485fa208a37f286b
Calvin <zxw880507@gmail.com>
2021-09-29 15:27:55 -0600

    rm file in dist

--- dist/basic/05-type_aliases.js
+++
@@ -1,2 +0,0 @@
-let codecademyCoordinates = [40, 43.2, "N", 73, 59.8, "W"];
-let bermudaTCoordinates = [25, 0, "N", 71, 0, "W"];
--- dist/misson/4-easy-pick.js
+++
@@ -1,9 +0,0 @@
-// Implement the built-in Pick<T, K> generic without using it.
-// Constructs a type by picking the set of properties K from T
-var test;
-(function (test) {
-    const todo = {
-        title: "Clean room",
-        completed: false,
-    };
-})(test || (test = {}));
